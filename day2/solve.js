0a7341df68e448066648502a9a0c4c0819b18c6a
Will Murphy <will.murphy@uk.ibm.com>
2019-12-02 10:51:22 +0000

    Replaced run() if statement with object index to make it smaller

--- day2/solve.js
+++ day2/solve.js
@@ -1,19 +1,16 @@
 const fs = require("fs");
 var sysmem = fs.readFileSync("input.txt","UTF-8").split(",");
 function operate(data) {
-	if (data.opcode == 1) {
-		sysmem[data.stor] = parseInt(sysmem[data.in1]) + parseInt(sysmem[data.in2]);
-	} else if (data.opcode == 2) {
-		sysmem[data.stor] = parseInt(sysmem[data.in1]) * parseInt(sysmem[data.in2]);
-	} else if (data.opcode == 99) {
-		return(sysmem);
-	}
+		sysmem[data.stor] = {1: parseInt(sysmem[data.in1]) + parseInt(sysmem[data.in2]),2: parseInt(sysmem[data.in1]) * parseInt(sysmem[data.in2])}[data.opcode];
 }
 function run(noun, verb) {
 	sysmem[1]=noun; sysmem[2]=verb;
 	for (var i = 0; i < sysmem.length; i+=4) {
-		out = operate({opcode:sysmem[i], in1:sysmem[i+1], in2:sysmem[i+2], stor:sysmem[i+3]});
-		if (out != undefined) { return out }
+		if (parseInt(sysmem[i]) == 99) {
+			return(sysmem);
+		} else {
+			out = operate({opcode:sysmem[i], in1:sysmem[i+1], in2:sysmem[i+2], stor:sysmem[i+3]});
+		}
 	}
 }
 console.log("Part 1: " + run(12,2)[0]);
