7cc69337b7731a9dd54756a9aed3354a4d2f24cc
Will Murphy <will.murphy@uk.ibm.com>
2019-12-02 10:39:50 +0000

    Added day 2, magic smoke restored

---
+++ day2/solve.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+var sysmem = fs.readFileSync("input.txt","UTF-8").split(",");
+function operate(data) {
+	if (data.opcode == 1) {
+		sysmem[data.stor] = parseInt(sysmem[data.in1]) + parseInt(sysmem[data.in2]);
+	} else if (data.opcode == 2) {
+		sysmem[data.stor] = parseInt(sysmem[data.in1]) * parseInt(sysmem[data.in2]);
+	} else if (data.opcode == 99) {
+		return(sysmem);
+	}
+}
+function run(noun, verb) {
+	sysmem[1]=noun; sysmem[2]=verb;
+	for (var i = 0; i < sysmem.length; i+=4) {
+		out = operate({opcode:sysmem[i], in1:sysmem[i+1], in2:sysmem[i+2], stor:sysmem[i+3]});
+		if (out != undefined) { return out }
+	}
+}
+console.log("Part 1: " + run(12,2)[0]);
+sysmem = fs.readFileSync("input.txt","UTF-8").split(",");
+for (var x = 0; x < 100; x++) {
+	for (var y = 0; y < 100; y++) {
+		if (parseInt(run(x,y)[0]) == 19690720) {
+			console.log("Part 2: " + x + y);
+		} 
+		sysmem = fs.readFileSync("input.txt","UTF-8").split(",");
+	}
+}
+
