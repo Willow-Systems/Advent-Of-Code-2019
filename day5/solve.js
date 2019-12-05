8a868616590ca045f853b930a7c9354466abfe65
Willow Systems <mail@willow.systems>
2019-12-05 11:00:08 +0000

    Removed unnecessary function

--- day5/solve.js
+++ day5/solve.js
@@ -11,7 +11,6 @@ function operate(data) {
 		var i1, i2;
 		i1 = (data.in1m == 1) ? data.in1 : sysmem[data.in1]
 		i2 = (data.in2m == 1) ? data.in2 : sysmem[data.in2]
-
 		if (data.opcode == 3 ) {
 			sysmem[data.in1] = readlineSync.question('Input: ');
 		} else if (data.opcode == 4) {
@@ -21,23 +20,20 @@ function operate(data) {
 		} else if (data.opcode == 6) {
 			i = (i1 == 0) ? parseInt(i2) : parseInt(i+3)
 		} else {
-			sysmem[data.stor] = {1: parseInt(i1) + parseInt(i2),2: parseInt(i1) * parseInt(i2), 7: sysmem[data.stor] = (parseInt(i1) < parseInt(i2)) ? 1 : 0, 8: (parseInt(i1) == parseInt(i2)) ? 1 : 0}[data.opcode];
+			sysmem[data.stor] = {1: parseInt(i1) + parseInt(i2),2: parseInt(i1) * parseInt(i2), 7: (parseInt(i1) < parseInt(i2)) ? 1 : 0, 8: (parseInt(i1) == parseInt(i2)) ? 1 : 0}[data.opcode];
 		}
 }
-function run() {
-	for (i = 0; i < sysmem.length; i=i) {
-		var testop = (sysmem[i] > 2) ? parseInt(sysmem[i].toString().substr(sysmem[i].toString().length-1,1)) : sysmem[i]
-		if (parseInt(testop) == 99) {
-			return(sysmem);
-		} else if (parseInt(testop) == 3 || parseInt(testop) == 4) {
-			operate({opcode:sysmem[i], in1:sysmem[i+1]});
-			i=i+2
-		} else if (parseInt(testop) == 5 || parseInt(testop) == 6) {
-			operate({opcode:sysmem[i], in1:sysmem[i+1], in2:sysmem[i+2]});
-		} else {
-			operate({opcode:sysmem[i], in1:sysmem[i+1], in2:sysmem[i+2], stor:sysmem[i+3]});
-			i += 4;
-		}
-	}
+for (i = 0; i < sysmem.length; i=i) {
+       var testop = (sysmem[i] > 2) ? parseInt(sysmem[i].toString().substr(sysmem[i].toString().length-1,1)) : sysmem[i]
+       if (parseInt(testop) == 99) {
+       	return(sysmem);
+       } else if (parseInt(testop) == 3 || parseInt(testop) == 4) {
+       	operate({opcode:sysmem[i], in1:sysmem[i+1]});
+       	i=i+2
+       } else if (parseInt(testop) == 5 || parseInt(testop) == 6) {
+       	operate({opcode:sysmem[i], in1:sysmem[i+1], in2:sysmem[i+2]});
+       } else {
+       	operate({opcode:sysmem[i], in1:sysmem[i+1], in2:sysmem[i+2], stor:sysmem[i+3]});
+       	i += 4;
+       }
 }
-run();
